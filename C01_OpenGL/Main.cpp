0549e2cf228d77d587e08842a3fad46d5de3ac0e
Idan Beck <idanbeck@gmail.com>
2019-09-02 16:58:35 -0700

    new color change branch (#1)

--- C01_OpenGL/Main.cpp
+++ C01_OpenGL/Main.cpp
@@ -43,7 +43,7 @@ const GLchar* fragmentShader = R"glsl(
 	out vec4 fragment;
 	void main()
 	{
-		fragment = vec4( 1.0, 0.0, 0.0, 1.0 );
+		fragment = vec4( 0.0, 1.0, 0.0, 1.0 );
 		return;
 	}
 )glsl";

2c4803b97ea0c79c0b50bb98a6ec55e80167ad6f
mxw4175 <mxw4175@IGM2000-28>
2019-09-24 20:11:45 -0400

    finished e05

--- E05/AppClass.cpp
+++ E05/AppClass.cpp
@@ -35,19 +35,22 @@ void Application::Display(void)
 	// Clear the screen
 	ClearScreen();
 
+	//make variable that wont be remade every frame and keeps value form prev frame
+	static float xInc = 0.0f;
+
 	//renders the cubes
 	//1st row
-	cubeArray[0]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-3.0f, 5.0f, 0.0f)));
-	cubeArray[1]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(3.0f, 5.0f, 0.0f)));
+	cubeArray[0]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-3.0f + xInc, 5.0f, 0.0f)));
+	cubeArray[1]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(3.0f + xInc, 5.0f, 0.0f)));
 
 	//2nd row
-	cubeArray[2]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-2.0f, 4.0f, 0.0f)));
-	cubeArray[3]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(2.0f, 4.0f, 0.0f)));
+	cubeArray[2]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-2.0f + xInc, 4.0f, 0.0f)));
+	cubeArray[3]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(2.0f + xInc, 4.0f, 0.0f)));
 
 	//3rd row
 	for (int i = 4; i < 11; i++)
 	{
-		cubeArray[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-3.0f + (i - 4), 3.0f, 0.0f)));
+		cubeArray[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-3.0f + (i - 4) + xInc, 3.0f, 0.0f)));
 	}
 
 	//4th row
@@ -63,28 +66,28 @@ void Application::Display(void)
 			pos += 1.0f;
 		}
 		
-		cubeArray[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(pos, 2.0f, 0.0f)));
+		cubeArray[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(pos + xInc, 2.0f, 0.0f)));
 	}
 
 	//5th row
 	for (int i = 18; i < 29; i++)
 	{
-		cubeArray[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-5.0f + (i - 18), 1.0f, 0.0f)));
+		cubeArray[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-5.0f + (i - 18) + xInc, 1.0f, 0.0f)));
 	}
 
 	//6th row
-	cubeArray[29]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-5.0f, 0.0f, 0.0f)));
+	cubeArray[29]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-5.0f + xInc, 0.0f, 0.0f)));
 	for (int i = 30; i < 37; i++)
 	{
-		cubeArray[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-3.0f + (i - 30), 0.0f, 0.0f)));
+		cubeArray[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-3.0f + (i - 30) + xInc, 0.0f, 0.0f)));
 	}
-	cubeArray[37]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(5.0f, 0.0f, 0.0f)));
+	cubeArray[37]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(5.0f + xInc, 0.0f, 0.0f)));
 
 	//7th row
-	cubeArray[38]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-5.0f, -1.0f, 0.0f)));
-	cubeArray[39]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-3.0f, -1.0f, 0.0f)));
-	cubeArray[40]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(3.0f, -1.0f, 0.0f)));
-	cubeArray[41]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(5.0f, -1.0f, 0.0f)));
+	cubeArray[38]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-5.0f + xInc, -1.0f, 0.0f)));
+	cubeArray[39]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(-3.0f + xInc, -1.0f, 0.0f)));
+	cubeArray[40]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(3.0f + xInc, -1.0f, 0.0f)));
+	cubeArray[41]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(5.0f + xInc, -1.0f, 0.0f)));
 
 	//8th row
 	for (int i = 42; i < 46; i++)
@@ -95,8 +98,11 @@ void Application::Display(void)
 			pos += 1.0f;
 		}
 
-		cubeArray[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(pos, -2.0f, 0.0f)));
+		cubeArray[i]->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3(pos + xInc, -2.0f, 0.0f)));
 	}
+
+	//increase the x
+	xInc += 0.1f;
 
 	//m_pMesh->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), ToMatrix4(m_qArcBall));
 	//m_pMesh1->Render(m_pCameraMngr->GetProjectionMatrix(), m_pCameraMngr->GetViewMatrix(), glm::translate(vector3( 3.0f, 0.0f, 0.0f)));
